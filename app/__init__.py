57409de12fb38c3557ec2119e42cde6b194c5d3b
Rafael-main <60815378+Rafael-main@users.noreply.github.com>
2021-07-28 00:38:53 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ import sqlite3
 from config import SECRET_KEY
 
 app = Flask(__name__)
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
 # cors = CORS(app, resources={r"/api/*": {origins:"*"}})
 CORS(app)
 
